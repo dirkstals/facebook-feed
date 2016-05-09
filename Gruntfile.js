2fe6630fc5281d77bf87377dfd600b3d5d1530a0
Dirk Stals <dirkstals@gmail.com>
2016-05-09 15:50:57 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+
+        useminPrepare: {
+            html: 'src/index.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+
+        usemin: {
+            html: ['dist/index.html']
+        },
+        
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html'
+                }
+            }
+        },
+
+        copy:{
+            html: {
+                src: 'src/index.html', dest: 'dist/index.html'
+            }
+        }
+    });
+
+    grunt.registerTask('default', [
+        'copy',
+        'useminPrepare',
+        'concat',
+        'uglify',
+        'cssmin',
+        'usemin',
+        'htmlmin'
+    ]);
+
+};
---
+++ index.js
@@ -0,0 +1,63 @@
+
+var debug = process.argv[process.argv.length - 1] == 'debug';
+
+var distFolder = debug ? '/src' : '/dist';
+
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+var config = require(process.cwd() + '/package.json');
+var facebookService = require('./src/assets/js/facebookService');
+
+var port = 3030;
+
+// API
+router.get('/', function(req, res){
+
+    res.json({ message: 'hooray! welcome to our api!' }); 
+});
+
+router.get('/users', function(req, res){
+
+    facebookService.getEventUsers(config.eventID, function(data){
+        res.json(data);    
+    });
+});
+
+router.get('/photos', function(req, res){
+
+    facebookService.getEventPhotos(config.eventID, function(data){
+        res.json(data);    
+    });
+});
+
+app.use('/api', router);
+
+
+// WEBSERVER
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + distFolder));
+
+app.get('/', function(req, res){
+
+    res.sendFile(__dirname + distFolder + '/index.html');
+});
+
+var server = app.listen(port, function(){
+    
+    console.log('listening on *:' + port);
+});
+
+
+// SOCKET.IO
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+    
+    console.log('New client connected!');
+
+    socket.emit('data', {
+        data: "This is my data"
+    });
+});

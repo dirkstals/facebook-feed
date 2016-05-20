e2892b88a0e898fcdaf3040cc046e8adca0425b7
Dirk Stals <dirkstals@gmail.com>
2016-05-20 12:07:26 +0200

    update slideshow on webhook

--- src/assets/js/feed.js
+++ src/assets/js/feed.js
@@ -7,23 +7,9 @@ var Feed = React.createClass({
 
         fetch('/api/feed').then(function(response) {
             return response.json().then(function(data){
+                
                 this.setState({feed: data.data});
-
-                this.state.feed.map(function(item){
-
-                    if(item.attachments && item.attachments.data && item.attachments.data.length > 0){
-
-                        item.attachments.data.map(function(attachment){
-                            if(attachment.subattachments && attachment.subattachments.data  && attachment.subattachments.data.length > 0){
-                                attachment.subattachments.data.map(function(subattachment){
-                                    slideshowReactElement.addNewImage(subattachment.media.image.src);    
-                                });
-                            }else{
-                                slideshowReactElement.addNewImage(attachment.media.image.src);
-                            }
-                        })                        
-                    }
-                });
+                this.createSlideshowFromPhotos();
             }.bind(this));
         }.bind(this));
     },
@@ -32,6 +18,26 @@ var Feed = React.createClass({
         Array.prototype.push.apply(posts.data, this.state.feed);
 
         this.setState({feed: posts.data});
+
+        this.createSlideshowFromPhotos();
+    },
+    createSlideshowFromPhotos: function(){
+
+         this.state.feed.map(function(item){
+
+            if(item.attachments && item.attachments.data && item.attachments.data.length > 0){
+
+                item.attachments.data.map(function(attachment){
+                    if(attachment.subattachments && attachment.subattachments.data  && attachment.subattachments.data.length > 0){
+                        attachment.subattachments.data.map(function(subattachment){
+                            slideshowReactElement.addNewImage(subattachment.media.image.src);    
+                        });
+                    }else{
+                        slideshowReactElement.addNewImage(attachment.media.image.src);
+                    }
+                })                        
+            }
+        });
     },
     render: function() {
         return React.createElement(
